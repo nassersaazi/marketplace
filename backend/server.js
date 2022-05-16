8ef749a321927eb1c1de102eed7a9b3961a53603
nassersaazi <nassersnoop@gmail.com>
2022-05-16 12:16:11 +0300

    Install nodemon for backend server

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,12 @@ import express from 'express';
 import data from './data.js';
 
 const app = express();
-// test
- // +app.get('/api/products', (req, res) => {
- // +  res.send(data.products);
- // +});
- // +
- // +const port = process.env.PORT || 5000;
- // +app.listen(port, () => {
- // +  console.log(`serve at http://localhost:${port}`);
- // +});
- // })
- // })
+
+app.get('/api/services', (req, res) => {
+  res.send(data.services);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
