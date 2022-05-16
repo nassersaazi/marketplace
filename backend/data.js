bd823fffe5f5bd686d88a7ad5ccdc28f7d8690c7
nassersaazi <nassersnoop@gmail.com>
2022-05-16 12:18:23 +0300

    Add data to be fetched by api

---
+++ backend/data.js
@@ -0,0 +1,66 @@
+const data = {
+  services: [
+    {
+      id: 1,
+      image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    },
+  {
+      
+      id: 2,
+      image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    },
+  {
+      
+      id: 3,
+      image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    },
+    {
+      id: 4,
+      image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    },
+{
+      id: 5,
+  image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    },
+{
+      id: 6,
+      image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    },
+{
+      id: 7,
+      image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    },
+  {
+      id: 8,
+      image: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0A883EAB54DDDC924018D08CF4555EE72B3E9A8C214BDADF29CB82B5E2275D7/scale?width=400&aspectRatio=1.78&format=jpeg',
+      name: 'Web Hosting',
+      date: '2022-04-21',
+      nren: 'MAREN',
+    }
+
+
+  ]
+};
+
+export default data;
