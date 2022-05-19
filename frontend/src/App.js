531c0dbaaac07066bad095848ed36cc9c9518b29
nassersaazi <nassersnoop@gmail.com>
2022-05-19 16:28:53 +0300

    Add responsive styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import {  Routes, Route } from 'react-router-dom';
-import { Header ,Home ,Services, Footer } from './components';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Header, Home, Services, Footer } from "./components";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/services" element={<Services />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/services" element={<Services />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Styles.js
+++ frontend/src/components/Styles.js
@@ -1,24 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Card = styled.div`
   display: grid;
   gap: 1rem;
-  grid-template-columns: repeat(4,1fr);
-img {
-  width: 100%;
-  border-radius: 4px;
-}
+  grid-template-columns: repeat(4, 1fr);
+  img {
+    width: 100%;
+    border-radius: 4px;
+  }
 
-@media only screen and (max-width: 1300px) {
-  grid-template-columns: repeat(2,1fr);
-}
+  @media only screen and (max-width: 1300px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-
-@media only screen and (max-width: 412px) {
-  grid-template-columns: 1fr;
-}
-`
-
+  @media only screen and (max-width: 412px) {
+    grid-template-columns: 1fr;
+  }
+`;
 
 export const Hero = styled.div`
   display: flex;
@@ -38,13 +36,12 @@ export const Hero = styled.div`
 `;
 
 export const Container = styled.div`
-
-    position: relative; 
-    top:72px;
-    overflow-x: hidden;
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-    font-weight:300;
-    padding: 0 calc(12vw);
-`
+  position: relative;
+  top: 72px;
+  overflow-x: hidden;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  font-weight: 300;
+  padding: 0 calc(12vw);
+`;
