5e7e2393c8ab5f22ed7bca6d7310f8cacc9236c5
nassersaazi <nassersnoop@gmail.com>
2022-05-26 23:19:31 +0300

    Get services from backend

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -1,9 +1,28 @@
-import styled from 'styled-components';
-import data from '../data';
+import React ,{ useEffect, useState} from "react";
 import { Container, Hero, CardGrid } from './Styles';
 import { Link  } from 'react-router-dom';
 
 const Services = () => {
+  
+  const [services, setServices] = useState([]);
+
+  const getServices = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/services");
+      const jsonData = await response.json();
+
+      setServices(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+
+  useEffect(() => {
+    getServices();
+  }, []);
+
+
   return (
     <Container>
       <Hero>
@@ -11,16 +30,15 @@ const Services = () => {
         <h2>Services</h2>
         <CardGrid>
         {
-          data.services.map((service) => (
-            <Link to="/service" className="service" key={service.id}>
-              <img src={service.image} alt={service.name}/>
+          services.map((service) => (
+            <Link to={`/service/${service.service_id}`} className="service" key={service.service_id}>
+              <img src={service.image_url} alt={service.s_name}/>
               <div className="service_description">
-              <p>In aenean posuere lorem risus nec. Tempor tincidunt aenean purus purus vestibulum nibh mi venenatis</p>
-              <p>In aenean posuere lorem risus nec. Tempor tincidunt aenean purus purus vestibulum nibh mi venenatis</p>
+                <p>{service.s_description}</p>
                 <div className="service_details">
-                  <p>{service.nren}</p>
-                  <p>Uploaded {service.date}</p>
-                  <p>Web Hosting</p>
+                  <p>{service.organisation}</p>
+                  <p>Uploaded {service.upload_date}</p>
+                  <p>{service.s_name}</p>
                 </div>
               </div>
             </Link>
