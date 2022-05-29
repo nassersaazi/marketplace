a53fd164d583f62d16beaf622447e4829b0388b4
nassersaazi <nassersnoop@gmail.com>
2022-05-29 20:49:53 +0300

    Add backend authentication setup

--- backend/database.sql
+++ backend/database.sql
@@ -20,3 +20,14 @@ CREATE TABLE services(
 
 ALTER TABLE services
 ADD image_url varchar(255);
+
+CREATE TABLE users(
+  user_id uuid DEFAULT uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(user_id)
+);
+
+INSERT INTO users (user_name, user_email, user_password) 
+VALUES ('henry', 'henryly213@gmail.com', 'kthl8822');
--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use("/auth", require("./routes/jwtAuth"));
 
 const today = new Date();
 
