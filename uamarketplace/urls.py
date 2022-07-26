41f89b372d7629a97391d1470b0e247d9cd1b8ea
nassersaazi <nassersnoop@gmail.com>
2022-07-26 23:38:52 +0300

    Test api with fake template file

--- uamarketplace/urls.py
+++ uamarketplace/urls.py
@@ -1,10 +1,10 @@
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('marketplace.urls',namespace='marketplace')),
-    path('api/', include('api.urls',namespace='api')),
+    path('', include(('marketplace.urls','marketplace'),namespace='marketplace')),
+    # path('api/', include(('api.urls','api'),namespace='api')),
 
 ]
