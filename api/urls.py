0fe01d3b68b1557896a72d9984848ab02b59c8c2
nassersaazi <nassersnoop@gmail.com>
2022-07-27 11:30:32 +0300

    Add URLs for initial service model

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 
-# from django.urls import path
-# from .views import PostList, PostDetail
+from django.urls import path
+from .views import ServiceList, ServiceDetail
 
-# app_name = 'api'
+app_name = 'api'
 
-# urlpatterns = [
-#     path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
-#     path('', PostList.as_view(), name='listcreate'),
-# ]
+urlpatterns = [
+    path('<int:pk>/', ServiceDetail.as_view(), name='detailcreate'),
+    path('', ServiceList.as_view(), name='listcreate'),
+]
