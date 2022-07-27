27f3aae99fc0bbd61e4e386d9fc95fd2165f23fd
nassersaazi <nassersnoop@gmail.com>
2022-07-27 11:35:02 +0300

    Create superadmin ,register models

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Service)
+class CreatedbyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'provider', 'category', 'accesstype', 'createdby','creationdate')
+    prepopulated_fields = {'slug': ('name',), }
+
+
+admin.site.register(models.Provider)
+
+admin.site.register(models.Category)
