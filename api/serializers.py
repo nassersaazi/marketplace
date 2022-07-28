f437582ec6a3b6f925a38260f38039750f961bf3
nassersaazi <nassersnoop@gmail.com>
2022-07-28 06:44:50 +0300

    Change folder structure

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from marketplace.models import Service
-
-
-class ServiceSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'name', 'provider', 'category', 'accesstype', 'createdby','creationdate')
-        model = Service
--- marketplace/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'marketplace'
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="marketplace/index.html")),
-]
