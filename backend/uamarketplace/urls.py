351729fac1d99420440acd6efd3fa7713bc04593
nassersaazi <nassersnoop@gmail.com>
2022-07-29 23:44:27 +0300

    Documentation: Add default schema class setting

--- backend/uamarketplace/urls.py
+++ backend/uamarketplace/urls.py
@@ -1,5 +1,7 @@
 from xml.etree.ElementInclude import include
 from django.contrib import admin
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -14,5 +16,11 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('project/docs/', include_docs_urls(title='MarketplaceAPI')),
+    path('project/schema', get_schema_view(
+        title="MarketplaceAPI",
+        description="API for the UA Marketplace",
+        version="1.0.0"
+    ), name='openapi-schema'),
 
 ]
